11d21b0f4eed3763c1562f9c37b9c06bb3b55892
keidsondesigner.com <keidsondesigner@gmail.com>
2026-02-17 18:14:37 -0400

    feat: 4 aula - Estrutura basica useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useState } from 'react'
 import Greeting from './components/Greeting'
 import MeuButton from './components/MeuButton'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   function handleClick() {
     alert('Clicado via props!')
   }
+
 
   return (
     <main>
       <h1>React 19 — Estudos</h1>
       <Greeting />
-      <hr />
+
+      <hr style={{ margin: '2rem 0' }} />
+
       <MeuButton
         variant="primary"
         title="Botão"
@@ -18,6 +24,19 @@ function App() {
       >
         Meu botão
       </MeuButton>
+
+      <br />
+      <br />
+
+      <MeuButton
+        variant="success"
+        title="Atualizar contador"
+        onClick={() => setCount(count + 1)}
+      >
+        Atualizar contador
+      </MeuButton>
+      <p>Contador: {count}</p>
+
     </main>
   )
 }
