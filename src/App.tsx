fccea8d9c20ddd45ba351612f6d3a2a406f30587
keidsondesigner.com <keidsondesigner@gmail.com>
2026-02-21 21:17:05 -0400

    feat: 8 aula - Múltiplos useEffect()

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ function App() {
 
   const [names, setNames] = useState(["Keidson", "karol", "Ana"]);
 
+  const [car, setCar] = useState("");
+
   // 6 aula - Filtrando dados da lista
   const [search, setSearch] = useState("");
 
@@ -15,9 +17,42 @@ function App() {
   }
 
   // 7 aula - Efeitos colaterais com useEffect()
+  // Veja a sequência dos seus logs:
+
+  // 🟢 montado e executado        ← 1ª montagem (StrictMode)
+  // Você clicou 0 vezes           ← useEffect([count]) roda
+  // Contador ou Carro atualizado  ← useEffect([count, car]) roda
+  // 🔴 desmontado (cleanup)       ← StrictMode DESMONTA de propósito
+  // 🟢 montado e executado        ← StrictMode REMONTA para testar
+  // Você clicou 0 vezes           ← tudo roda de novo
+  // Contador ou Carro atualizado  ← tudo roda de novo
+
+  // 8 aula - Múltiplos useEffect() quando usar cada um?
+  // Use um useEffect() para cada efeito colateral.
+  // Exemplo: um useEffect para buscar dados, outro para manipular o DOM, etc.
+  // Não misture efeitos colaterais diferentes no mesmo useEffect().
+  // Cada useEffect() tem seu próprio ciclo de vida.
+  // Cada useEffect() tem sua própria limpeza.
+  // Cada useEffect() tem sua própria ordem de execução.
+  // Cada useEffect() tem sua própria ordem de limpeza.
+
   useEffect(() => {
-    console.log(document.title = `Você clicou ${count} vezes`);
+    console.log('🟢 montado e executado')
+
+    return () => {
+      console.log('🔴 desmontado (cleanup)')
+      // aqui vai o codigo de limpeza
+      // Exemplo: cancelar fetch, limpar timer, remover EventListener e Subscriptions.
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log(document.title = `Você clicou, e atualizou ${count} vezes`);
   }, [count]);
+
+  useEffect(() => {
+    console.log("Contador ou Carro foi atualizado");
+  }, [count, car]);
 
 
   return (
@@ -57,6 +92,17 @@ function App() {
         onClick={() => setCount(0)}
       >
         Limpar contador
+      </MeuButton>
+
+      <br />
+      <br />
+
+      <MeuButton
+        variant="primary"
+        title="Adicionar carro"
+        onClick={() => setCar("Gol Quadrado")}
+      >
+        Adicionar ao carro
       </MeuButton>
 
       <br />
